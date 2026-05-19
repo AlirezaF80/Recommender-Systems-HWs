d9e64d6bfe070e0d0912900717bdfcf44520cf93
AlirezaF <alirezafarzaneh138@gmail.com>
2026-05-19 21:07:51 +0330

    Optimize system 2

--- src/user_cf.py
+++ src/user_cf.py
@@ -2,9 +2,11 @@
 
 import math
 
+from tqdm import tqdm
+
 from metrics import N_VALUES, average_metrics_per_user
 from preprocess import MOVIEID_IDX
-from ranking import group_by_user, top_n_from_scores
+from ranking import group_by_user
 
 K_VALUES = [2, 5, 10]
 
@@ -24,21 +26,55 @@ def build_profiles(train):
     return ratings, user_means
 
 
+def build_movie_index(all_ratings):
+    """Invert profiles: movie_id -> {user_id: rating}."""
+    movie_users = {}
+    for user_id, movies in all_ratings.items():
+        for movie_id, rating in movies.items():
+            if movie_id not in movie_users:
+                movie_users[movie_id] = {}
+            movie_users[movie_id][user_id] = rating
+    return movie_users
+
+
+def co_rating_counts(user_id, ratings_u, movie_users):
+    """How many train movies each other user shares with user_id."""
+    overlap = {}
+    for movie_id in ratings_u:
+        for other_id in movie_users.get(movie_id, {}):
+            if other_id == user_id:
+                continue
+            overlap[other_id] = overlap.get(other_id, 0) + 1
+    return overlap
+
+
 def pearson_sim(ratings_u, ratings_v):
     """Pearson correlation on co-rated items; 0 if fewer than 2 co-ratings."""
-    common = set(ratings_u) & set(ratings_v)
-    if len(common) < 2:
+    common_pairs = []
+    if len(ratings_u) <= len(ratings_v):
+        for movie_id, rating_u in ratings_u.items():
+            rating_v = ratings_v.get(movie_id)
+            if rating_v is not None:
+                common_pairs.append((rating_u, rating_v))
+    else:
+        for movie_id, rating_v in ratings_v.items():
+            rating_u = ratings_u.get(movie_id)
+            if rating_u is not None:
+                common_pairs.append((rating_u, rating_v))
+
+    n = len(common_pairs)
+    if n < 2:
         return 0.0
 
-    mean_u = sum(ratings_u[i] for i in common) / len(common)
-    mean_v = sum(ratings_v[i] for i in common) / len(common)
+    mean_u = sum(rating_u for rating_u, _ in common_pairs) / n
+    mean_v = sum(rating_v for _, rating_v in common_pairs) / n
 
     num = 0.0
     den_u = 0.0
     den_v = 0.0
-    for movie_id in common:
-        diff_u = ratings_u[movie_id] - mean_u
-        diff_v = ratings_v[movie_id] - mean_v
+    for rating_u, rating_v in common_pairs:
+        diff_u = rating_u - mean_u
+        diff_v = rating_v - mean_v
         num += diff_u * diff_v
         den_u += diff_u * diff_u
         den_v += diff_v * diff_v
@@ -48,17 +84,27 @@ def pearson_sim(ratings_u, ratings_v):
     return num / (math.sqrt(den_u) * math.sqrt(den_v))
 
 
-def top_k_neighbors(user_id, all_ratings, k):
-    """Top-k most similar users by Pearson (exclude self)."""
-    sims = []
+def ranked_neighbor_sims(user_id, all_ratings, movie_users):
+    """All other users sorted by Pearson (exclude self)."""
     ratings_u = all_ratings[user_id]
-    for other_id, ratings_v in all_ratings.items():
+    overlap = co_rating_counts(user_id, ratings_u, movie_users)
+    sims = []
+    for other_id in all_ratings:
         if other_id == user_id:
             continue
-        sim = pearson_sim(ratings_u, ratings_v)
-        sims.append((other_id, sim))
+        if overlap.get(other_id, 0) < 2:
+            sims.append((other_id, 0.0))
+        else:
+            sims.append((other_id, pearson_sim(ratings_u, all_ratings[other_id])))
     sims.sort(key=lambda x: (-x[1], x[0]))
-    return sims[:k]
+    return sims
+
+
+def top_k_neighbors(user_id, all_ratings, k, movie_users=None):
+    """Top-k most similar users by Pearson (exclude self)."""
+    if movie_users is None:
+        movie_users = build_movie_index(all_ratings)
+    return ranked_neighbor_sims(user_id, all_ratings, movie_users)[:k]
 
 
 def predict_rating(user_id, movie_id, neighbors, all_ratings, user_means):
@@ -89,6 +135,12 @@ def predict_test_user(user_id, test_movies, all_ratings, user_means, neighbors):
     return scores
 
 
+def ranked_movie_ids_from_scores(movie_scores):
+    """All movieIds by predicted score (highest first), for slicing top-n."""
+    ranked = sorted(movie_scores.items(), key=lambda x: (x[1], -x[0]), reverse=True)
+    return [movie_id for movie_id, _ in ranked]
+
+
 def evaluate_user_cf(result, k_values=None, n_values=None):
     """Return {k: {n: {hit_rate, precision}}} over test users."""
     if k_values is None:
@@ -97,17 +149,28 @@ def evaluate_user_cf(result, k_values=None, n_values=None):
         n_values = N_VALUES
 
     all_ratings, user_means = build_profiles(result["train"])
+    movie_users = build_movie_index(all_ratings)
     test_by_user = group_by_user(result["test"])
 
-    metrics = {}
-    for k in k_values:
-        metrics[k] = {}
+    ranked_neighbors_by_user = {}
+    for user_id in tqdm(test_by_user, desc="User CF neighbors", unit="user"):
+        ranked_neighbors_by_user[user_id] = ranked_neighbor_sims(
+            user_id, all_ratings, movie_users
+        )
+
+    metrics = {k: {} for k in k_values}
+    for k in tqdm(k_values, desc="User CF k", unit="k"):
         neighbors_by_user = {
-            user_id: top_k_neighbors(user_id, all_ratings, k)
+            user_id: ranked_neighbors_by_user[user_id][:k]
             for user_id in test_by_user
         }
         scores_by_user = {}
-        for user_id, user_rows in test_by_user.items():
+        for user_id, user_rows in tqdm(
+            test_by_user.items(),
+            desc=f"User CF predict (k={k})",
+            unit="user",
+            leave=False,
+        ):
             test_movies = list({r[MOVIEID_IDX] for r in user_rows})
             scores_by_user[user_id] = predict_test_user(
                 user_id,
@@ -117,9 +180,13 @@ def evaluate_user_cf(result, k_values=None, n_values=None):
                 neighbors_by_user[user_id],
             )
 
+        ranked_movies_by_user = {
+            user_id: ranked_movie_ids_from_scores(scores)
+            for user_id, scores in scores_by_user.items()
+        }
         for n in n_values:
             recommended_by_user = {
-                user_id: top_n_from_scores(scores_by_user[user_id], n)
+                user_id: ranked_movies_by_user[user_id][:n]
                 for user_id in test_by_user
             }
             metrics[k][n] = average_metrics_per_user(
